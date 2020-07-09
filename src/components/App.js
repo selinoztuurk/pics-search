01c8496e74de1a0cbb646b6f7a0780d5a35d539a
Selin <selinozturk@Selin-MacBook-Pro.local>
2020-07-09 10:14:45 +0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  onSearchSubmit(term) {}
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "15px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
