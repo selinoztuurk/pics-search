1ed47116a6913093327f74cd424397a74223b33c
Selin <selinozturk@Selin-MacBook-Pro.local>
2020-07-09 12:36:13 +0300

    V1 completed

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,7 @@ const ImageList = (props) => {
   const images = props.images.map(({ description, id, urls }) => {
     return <img key={id} alt={description} src={urls.small} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
